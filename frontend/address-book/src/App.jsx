af1eea44495405c6b1349d75d93d9e7e249011a9
DanielaParra <dparralp396@gmail.com>
2023-05-01 20:56:25 -0400

    Initialized React app, folder structure

---
+++ frontend/address-book/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LandingPage from "./views/LandingPage";
+
+function App() {
+  return (
+    <>
+      <h1>Pawnee Parks and Recs Dept.</h1>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
