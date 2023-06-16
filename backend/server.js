2f47540797f75c3df0d30255d0ca1d1347bab706
DanielaParra <dparralp396@gmail.com>
2023-06-15 20:45:48 -0400

    Tweaks from working w/ Eric + Benjamin

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
@@ -6,21 +6,21 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // requires configuration of mongoose set up to MongoDB
 require('./db/mongoose.config');
-// server requires the routes from dedicated customer route file
-require('./routes/customers.routes')(app);
 
 const app = express();
+// server requires the routes from dedicated customer route file
+require('./routes/customers.routes')(app);
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:5172' }));
 // this recognizes the incomming request as a json object
 app.use(express.json());
 // needed fo post and put routes to tell the server to accept the json given by the req body from the frontend form submission
-app.use(express.urlencoded({ extended: true })); 
-app.get("/", (request, response) => {
-    response.send("Hi there");
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (request, response) => {
+	response.send('Hi there');
 });
-// setting the port to listen to requests to match the front end port 
-const port = 5173;
+// setting the port to listen to requests to match the front end port
+const port = 5172;
 app.listen(port, () => {
-    console.log(`Listen on the port ${port}...`);
+	console.log(`Listen on the port ${port}...`);
 });
