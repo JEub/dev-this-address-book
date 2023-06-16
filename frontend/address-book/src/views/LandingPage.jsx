14a2f3563f3d68468f5b69b8811e2a6b7c6530d9
TheResinger <mororwbenjamin42@gmail.com>
2023-06-16 11:41:25 -0400

    re-enabled login

--- frontend/address-book/src/views/LandingPage.jsx
+++ frontend/address-book/src/views/LandingPage.jsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 import Nav from "../components/Nav";
 import Main from "../components/Main";
-import {useRecoilState, useRecoilValue} from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import UserModal from "../components/UserModal";
 import UserLogin from "../components/Login";
 import { currentModalState, currentModalData } from '../reactstates';
 
 const LandingPage = () => {
-    const [showModal, setShowModal] = useRecoilState(currentModalState);
-    const [showLogin, setShowLogin] = useState(true);
+  const [showModal, setShowModal] = useRecoilState(currentModalState);
+  const [showLogin, setShowLogin] = useState(true);
 
   return (
     <>
       <div>LandingPage</div>
-      <Nav 
-        showModal = {showModal}
-        setShowModal = {setShowModal}
-        />
+      <UserLogin showLogin={showLogin} setShowLogin={setShowLogin} />
+      <Nav
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
       {/* import atom into main container to read */}
       <Main />
-      <UserModal 
-        showModal = {showModal}
-        setShowModal = {setShowModal}/>
+      <UserModal
+        showModal={showModal}
+        setShowModal={setShowModal} />
     </>
-  ); 
+  );
 };
 
 export default LandingPage;
